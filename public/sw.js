7d0227e63852f7e9a4045db98a9cc7feba123c40
Stanley Masinde <stanmasinde@gmail.com>
2022-05-22 14:17:37 +0300

    layout fixes

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * The main service worker file. 
  * Author: Stanley Masinde
  */
-const version = 'v1'
+const version = 'v2'
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(version).then(cache => {
@@ -21,6 +21,8 @@ self.addEventListener('install', event => {
             ])
         })
     )
+
+	self.skipWaiting()
 })
 
 // // ----------------------------------------------------------------------------
