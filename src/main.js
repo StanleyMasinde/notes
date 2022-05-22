d3ada9aef93e33c523c61321775029844977d648
Stanley Masinde <stanmasinde@gmail.com>
2022-05-22 13:12:59 +0300

    works offline

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(reg => {
+        console.log(reg);
+    })
+    .catch(er => {
+        console.error(er);
+    })
+}
+
 createApp(App).mount('#app')
