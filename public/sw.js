9885810e8eff96b3161ba0bf7568139dc0299140
Stanley Masinde <stanmasinde@gmail.com>
2022-05-22 16:39:15 +0300

    Search functionality for notes

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * The main service worker file. 
  * Author: Stanley Masinde
  */
-const version = 'v2'
+const version = 'v3'
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(version).then(cache => {
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="text-center dark:bg-black dark:text-white h-64 flex justify-center align-middle flex-col px-2">
+  <div @click="navMenu = false"
+    class="text-center dark:bg-black dark:text-white h-64 flex justify-center align-middle flex-col px-2">
     <div>
       <h1 class=" text-3xl">All notes</h1>
       <p class=" font-light">{{ notes.length }} {{ notes.length === 1 ? 'note' : 'notes' }}</p>
@@ -18,22 +19,31 @@
 
     <ul class="flex">
       <li class="mx-3">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-          stroke-width="2">
+        <svg @click="showSearchMenu = true" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-          stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
-        </svg>
+        <div class="relative">
+          <div v-show="navMenu" class="absolute bg-gray-200 dark:bg-gray-700 right-0 p-3 rounded-xl w-28">
+            <ul>
+              <li>Edit</li>
+              <li>View</li>
+            </ul>
+          </div>
+          <svg @click="navMenu = true" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+          </svg>
+        </div>
       </li>
     </ul>
   </nav>
 
-  <div v-if="notes.length === 0" class="text-center h-[100vh] flex flex-col px-2">
+  <div @click="navMenu = false" v-if="notes.length === 0"
+    class="text-center h-[95vh] flex flex-col px-2 dark:bg-black dark:text-white">
     <div>
       <h1 class=" text-2xl font-semibold">You have no notes</h1>
       <p class=" text-lg font-thin">Click the button on the bottom right to create the first one</p>
@@ -47,7 +57,9 @@
       </div>
       <div class="text-center">
         <h1 class=" font-semibold">{{ note.title }}</h1>
-        <p class=" font-thin text-sm">{{ new Date(note.createdAt).toLocaleDateString() }} {{ new Date(note.createdAt).toLocaleTimeString() }}</p>
+        <p class=" font-thin text-sm">{{ new Date(note.createdAt).toLocaleDateString() }} {{ new
+            Date(note.createdAt).toLocaleTimeString()
+        }}</p>
       </div>
     </div>
   </section>
@@ -92,8 +104,8 @@
     <nav class="py-3">
       <ul class="grid grid-cols-11">
         <li>
-          <svg @click="updateNote(currentNote)" xmlns="http://www.w3.org/2000/svg" class="h-12 w-10 text-2xl" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <svg @click="updateNote(currentNote)" xmlns="http://www.w3.org/2000/svg" class="h-12 w-10 text-2xl"
+            fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
           </svg>
         </li>
@@ -108,15 +120,55 @@
       class="w-screen h-screen dark:bg-black border-0 focus:ring-0 caret-[#ffe500] text-xl"></textarea>
   </section>
   <!--/Form for editing a note-->
+
+
+  <!--Search Menu-->
+  <div v-if="showSearchMenu" class="fixed top-0 left-0 w-full h-screen bg-white dark:bg-black dark:text-white">
+    <nav class="py-3">
+      <ul class="grid grid-cols-11">
+        <li>
+          <svg @click="showSearchMenu = false; searchResults = []; searchQuery = null"
+            xmlns="http://www.w3.org/2000/svg" class="h-12 w-10 text-2xl" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+          </svg>
+        </li>
+        <li class=" col-span-9">
+          <input autofocus v-model="searchQuery" type="text" name="title" id="title"
+            class="border-0 w-full caret-[#ffe500] dark:bg-black focus:ring-0 placeholder:font-semibold placeholder:text-2xl text-2xl"
+            placeholder="Search">
+        </li>
+      </ul>
+    </nav>
+
+    <div class="grid grid-cols-2 px-2 gap-1 dark:bg-black pt-2 dark:text-white min-h-[100vh]">
+      <div v-for="(note, i) in searchResults" :key="i" @click.prevent="editNote(note)">
+        <div class="border-2 h-52 rounded-2xl dark:bg-gray-900 dark:text-white px-2 py-2">
+          <p class=" text-xs">{{ note.body }}</p>
+        </div>
+        <div class="text-center">
+          <h1 class=" font-semibold">{{ note.title }}</h1>
+          <p class=" font-thin text-sm">{{ new Date(note.createdAt).toLocaleDateString() }} {{ new
+              Date(note.createdAt).toLocaleTimeString()
+          }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/Search menu-->
 </template>
 
 <script setup>
 import { reactive, ref } from '@vue/reactivity';
-import { onMounted } from '@vue/runtime-core';
+import { onMounted, watch } from '@vue/runtime-core';
 import { db } from './db';
 
 const showCreateForm = ref(false)
 const showEditForm = ref(false)
+const showSearchMenu = ref(false)
+const searchQuery = ref('')
+const searchResults = ref([])
+const navMenu = ref(false)
 const notes = ref([])
 const newNoteData = reactive({
   title: null,
@@ -134,6 +186,25 @@ const getNotes = async () => {
   return
 }
 
+const searchNotes = async () => {
+  if (searchQuery.value.length === 0) {
+    searchResults.value = []
+    return
+  }
+  const nts = await db.notes.where('title')
+    .startsWithIgnoreCase(searchQuery.value)
+    .or('body')
+    .startsWithIgnoreCase(searchQuery.value)
+    .reverse()
+    .toArray()
+  searchResults.value = nts
+  return
+}
+
+watch(searchQuery, () => {
+  searchNotes()
+})
+
 const editNote = async (note) => {
   const nt = await db.notes.where({
     id: note.id
@@ -141,6 +212,7 @@ const editNote = async (note) => {
   currentNote.id = nt.id
   currentNote.title = nt.title
   currentNote.body = nt.body
+  navMenu.value = false
   showEditForm.value = true
 }
 
@@ -170,6 +242,7 @@ const toggleCreateForm = () => {
       newNoteData.body = null
     }
   }
+  navMenu.value = false
   showCreateForm.value = !showCreateForm.value
 }
 </script>
