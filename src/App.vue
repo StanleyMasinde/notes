d839c45820135aa7a1413bd13c6260499ab88c4e
Stanley Masinde <stanmasinde@gmail.com>
2022-05-23 14:52:01 +0300

    Added screeshots and share target

--- src/App.vue
+++ src/App.vue
@@ -115,8 +115,8 @@
             placeholder="Title">
         </li>
         <li>
-          <svg @click="deleteNote(currentNote)" xmlns="http://www.w3.org/2000/svg" class="h-12 w-8 text-2xl" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-            stroke-width="2">
+          <svg @click="deleteNote(currentNote)" xmlns="http://www.w3.org/2000/svg" class="h-12 w-8 text-2xl" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
           </svg>
@@ -241,8 +241,16 @@ const updateNote = async (note) => {
 onMounted(() => {
   getNotes()
   console.log(location.search)
-  if(location.search === '?new') {
+  if (location.search === '?new') {
     showCreateForm.value = true
+  }
+
+  const parsedUrl = new URL(window.location);
+  if (parsedUrl.searchParams.get('shared_title')) {
+    showCreateForm.value = true
+    newNoteData.title = parsedUrl.searchParams.get('shared_title')
+    newNoteData.body = parsedUrl.searchParams.get('shared_body')
+    newNoteData.body = newNoteData.body + '\n\n' + 'Shared from ' + parsedUrl.searchParams.get('shared_url')
   }
 })
 
