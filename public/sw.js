b6535039bb0af6ef98811a788cc08332adc43f75
Stanley Masinde <stanmasinde@gmail.com>
2023-06-23 18:06:59 +0300

    chore(offline): bump the cache version to 15

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * The main service worker file. 
  * Author: Stanley Masinde
  */
-const version = 'v14'
+const version = 'v15'
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(version).then(async(cache) => {
