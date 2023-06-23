aef7bf901d8f532b8a0d6d17a938d6dd97ded2a6
Stanley Masinde <stanmasinde@gmail.com>
2023-06-23 22:29:03 +0300

    dark mode support

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -54,9 +54,9 @@ const createUrl = (url: string) => {
         </svg>
     </button>
 
-    <div v-if="showShareDialog" class="fixed inset-0 grid place-items-center backdrop-blur backdrop-brightness-50">
+    <div v-if="showShareDialog" class="fixed inset-0 grid place-items-center backdrop-blur backdrop-brightness-50 dark:backdrop-brightness-100">
         <div>
-            <div class="bg-white/50 w-96 rounded-xl shadow-md p-5 flex flex-col gap-10 backdrop-blur-lg ">
+            <div class="bg-white/50 dark:bg-black/50 dark:shadow-white dark:shadow w-96 rounded-xl shadow-md p-5 flex flex-col gap-10 backdrop-blur-lg ">
                 <div>
                     <h1 class=" text-xl font-bold">Share this note</h1>
                 </div>
@@ -117,7 +117,7 @@ const createUrl = (url: string) => {
                     <li title="Clipboard">
                         <button @click="copyToClipboard()">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="h-10">
+                                stroke="currentColor" class="h-10 dark:text-white">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
                             </svg>
@@ -136,7 +136,7 @@ const createUrl = (url: string) => {
 
     <!--Clipboard toast-->
     <div v-if="showToast" class="fixed inset-x-0 bottom-2 flex justify-center">
-        <div class="relative bg-white/50 px-5 shadow-md rounded-lg w-56 py-4">
+        <div class="relative bg-white/50 dark:bg-black/50 dark:text-white dark:shadow-white px-5 shadow-md dark:shadow rounded-lg w-56 py-4">
             <h1>Note copied to clipboard!</h1>
         </div>
     </div>
